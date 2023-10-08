4f972b4f87b324c6d984b2c055c977c7865e77ed
Arun Gound <arun00gound@gmail.com>
2023-10-08 17:48:54 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
+import "./App.css";
 
 function App() {
+  const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>Auth0-Demo</h2>
+        <div className="user">
+          {isAuthenticated && <h3>Hi there, {user.name}</h3>}
+          {isAuthenticated ? (
+            <button
+              onClick={() =>
+                logout({ logoutParams: { returnTo: window.location.origin } })
+              }
+            >
+              Logout
+            </button>
+          ) : (
+            <button onClick={() => loginWithRedirect()}>Log In</button>
+          )}
+        </div>
       </header>
+      <main>
+        {isAuthenticated ? (
+          <h3>You're now logged in!</h3>
+        ) : (
+          <h3>You're not logged in!</h3>
+        )}
+      </main>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Auth0Provider
+      domain="dev-oiv38pq7p1h400my.us.auth0.com"
+      clientId="z2iwM7vNnSmZeqUW2ZLfS3Ikkc2OmPFW"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
